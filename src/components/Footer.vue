eb647f506656ba8ed5287462bfb8633eb8ad35ac
ximenes-oss <ximenesmarco26@gmail.com>
2021-09-11 15:29:35 +0900

    initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,73 @@
+<template>
+	<footer>
+		<div class="container-fluid" style="background: white;">
+			<div class="row p-4">
+				<div class="col-md-4">
+					<h5>SOBRE</h5>
+				<hr>
+					<p>
+						Eskola Mak Detemina Ema nia Konesementu Sencsia no Departamentu no liu husi prosseso Formal.
+					</p>
+				</div>
+				
+				<div class="col-md-4">
+					<h5>TAGS</h5>
+				<hr>
+					<div v-if="tags.length > 0">
+					<router-link :to="{name: 'detail_tag', params: {
+					slug: tag.slug }}" v-for="tag in tags" :key="tag.id" class="btn btn-sm btn-outline-secondary mb-2 me-2">{{ tag.name.toUpperCase() }}</router-link>
+					</div>
+					
+					<div v-else class="text-center">
+					<ListLoader/>
+					</div>
+				</div>
+				
+			<div class="col-md-4">
+				<h5>KONTAK</h5>
+			<hr>
+				<p>
+					<i class="fa fa-map-marker" aria-hidden="true"></i> EL. Lugabuty, Address Quelicai-Baucau-DILI, TIMOR-LESTE
+					<i class="fas fa-phone"></i> +670 77154409
+				</p>
+			</div>
+			</div>
+		</div>
+		
+		<div class="container-fluid bg-dark">
+			<div class="row p-3">
+				<div class="text-center text-white font-weight-bold">
+					Copyright © 2021	 EBG TIMOR_LESTE. All rights reserved.
+				</div>
+			</div>
+		</div>
+	</footer>
+</template>
+
+
+<script>
+	//import content loader
+	import {
+	ListLoader
+	} from 'vue-content-loader'
+	
+	//import axios
+	import axios from 'axios' 
+    export default {
+		name: 'Footer',
+		components: {
+		//loader component
+		ListLoader
+		},
+		data() {
+		return {
+		tags: []
+		}
+		},
+		created() {
+		axios.get('/api/tag').then(response => {
+		this.tags = response.data.data.data
+		});
+		}
+	}
+</script>
---
+++ src/views/contact/Index.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="post-index">
+        <!-- header -->
+        <Header />
+        <!-- end header -->
+
+        <!-- main content -->
+        <header class="pt-5 border-bottom bg-light">
+            <div class="container pt-md-1 pb-md-1">
+                <h1 class="bd-title mt-4 font-weight-bold"><i class="fa fa-phone" aria-hidden="true"></i> KONTAK</h1>
+                <p class="bd-lead">Kontak EBG Timor-Leste.</p>
+            </div>
+        </header>
+
+        <!-- breadcrumb -->
+        <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><router-link :to="{ name: 'home' }" class="text-decoration-none"><i class="fa fa-home"></i> Home</router-link></li>
+            <li class="breadcrumb-item"><router-link :to="{ name: 'contact' }" class="text-decoration-none"><i class="fa fa-phone"></i> Kontak</router-link></li>
+        </ol>
+        </nav>
+        <!-- end breadcrumb -->
+
+        <div class="container-fluid mt-5 mb-5">
+
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="card border-0 shadow-sm rounded">
+                        <div class="card-body">
+                            <iframe src="https://www.google.com/maps/d/embed?mid=1gmkyYaoOZm672fIV1m1p8k_ZFSZ9vTjb" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="card border-0 shadow-sm rounded">
+                        <div class="card-body">
+                            <h3>Contacto Nos</h3>
+                            <hr>
+                            <p>
+                                <i class="fa fa-map-marker" aria-hidden="true"></i> DILI. Fatuhada-Marconi,Timor-Leste
+                                <br>
+                                <br>
+                                <i class="fas fa-phone"></i> +670 77154409
+                                <br>
+                                <br>
+                                <i class="fas fa-envelope"></i> ximenesmarco26@gmail.com
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- main content -->
+
+        <!-- footer -->
+        <Footer />
+        <!-- end footer -->
+    </div>
+</template>
+
+<script>
+    //import component
+    import Header from "@/components/Header"
+    import Footer from "@/components/Footer"
+
+    export default {
+        name: 'ContactIndex',
+        components: {
+            Header,
+            Footer
+        }
+    }
+</script>
